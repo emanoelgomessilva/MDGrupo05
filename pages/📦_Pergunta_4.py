c294e08845367f5caefbd5cbdd065f9be1e9832c
emanoelgomessilva <emanoel20092009@gmail.com>
2024-02-11 12:23:52 -0300

    filtros das perguntas 3 e 4

--- pages/📦_Pergunta_4.py
+++ pages/📦_Pergunta_4.py
@@ -43,14 +43,65 @@ def Pergunta_4():
                             'SE',
                             'TO',
                         ])
+    
+    filtro_2 = st.sidebar.selectbox('De:', [
+                            '2017',
+                            '2018',
+                            '2019',
+                            '2020',
+                            '2021',
+                            '2022',
+                        ])
+    
+    filtro_3 = st.sidebar.selectbox('Até:', [
+                            '2017',
+                            '2018',
+                            '2019',
+                            '2020',
+                            '2021',
+                            '2022',
+                        ])
+    
+    consulta_filtro_orgao_superior = "select NOME_ORGAO_SUPERIOR from dw_convenios.dim_orgao_superior limit 0, 1000"
+    lista_orgao_superior = pd.read_sql_query(consulta_filtro_orgao_superior, conexao)
+    
+    filtro_4 = st.sidebar.selectbox('Selecione um órgão superior', lista_orgao_superior)
 
-    consulta_sql = "SELECT dos.NOME_ORGAO_SUPERIOR, AVG(VALOR_CONVENIO) as valor_convenio FROM dim_orgao_superior as dos inner join fatoconvenios as fc inner join dimdata as dt on dos.sk_orgao_superior = fc.k_Orgao_Superior and dt.keyData = fc.k_Data and dt.ano_id >= 2019 and dt.ano_id <= 2022 inner join dim_municipio as dm on dm.sk_municipio = fc.k_Municipio and dm.UF ="+"'"+str(filtro_1)+"'"
+    consulta_filtro_orgao_concedente = "select NOME_ORGAO_CONCEDENTE from dw_convenios.dim_orgao_concedente limit 0, 1000"
+    lista_orgao_concedente = pd.read_sql_query(consulta_filtro_orgao_concedente, conexao)
+    
+    filtro_5 = st.sidebar.selectbox('Selecione um órgão concedente', lista_orgao_concedente)
+
+    consulta_filtro_ug_concedente = "select NOME_UG_CONCEDENTE from dw_convenios.dim_ug_concedente limit 0, 1000"
+    lista_ug_concedente = pd.read_sql_query(consulta_filtro_ug_concedente, conexao)
+    
+    filtro_6 = st.sidebar.selectbox('Selecione uma UG concedente', lista_ug_concedente)
+
+    consulta_sql = "SELECT dos.NOME_ORGAO_SUPERIOR, AVG(VALOR_CONVENIO) as valor_convenio FROM dim_orgao_superior as dos inner join fatoconvenios as fc inner join dim_ug_concedente as ugc on ugc.sk_ug_concedente = fc.k_UG_Concedente inner join dim_orgao_concedente as doc on doc.sk_orgao_concedente = fc.k_Orgao_Concedente inner join dimdata as dt on dos.sk_orgao_superior = fc.k_Orgao_Superior and dt.keyData = fc.k_Data and dt.ano_id >= "+ filtro_2 + " and dt.ano_id <= " + filtro_3 +" inner join dim_municipio as dm on dm.sk_municipio = fc.k_Municipio and dm.UF ="+"'"+str(filtro_1)+"'"
+
+    if filtro_4 != None:
+        consulta_sql += " and dos.NOME_ORGAO_SUPERIOR ="+"'"+filtro_4+"'"
+
+    if filtro_5 != None:
+        consulta_sql += " and doc.NOME_ORGAO_CONCEDENTE ="+"'"+filtro_5+"'"
+
+    if filtro_6 != None:
+       consulta_sql += " and ugc.NOME_UG_CONCEDENTE ="+"'"+filtro_6+"'"
 
     consulta_sql+= " GROUP BY dos.NOME_ORGAO_SUPERIOR ORDER BY valor_convenio DESC"
     
     dados = pd.read_sql_query(consulta_sql, conexao)
 
-    consulta_sql_2 = "SELECT dos.NOME_ORGAO_SUPERIOR, AVG(VALOR_CONVENIO) as valor_convenio FROM dim_orgao_superior as dos inner join fatoconvenios as fc inner join dimdata as dt on dos.sk_orgao_superior = fc.k_Orgao_Superior and dt.keyData = fc.k_Data and dt.ano_id >= 2017 and dt.ano_id <= 2019 inner join dim_municipio as dm on dm.sk_municipio = fc.k_Municipio and dm.UF ="+"'"+str(filtro_1)+"'"
+    consulta_sql_2 = "SELECT dos.NOME_ORGAO_SUPERIOR, AVG(VALOR_CONVENIO) as valor_convenio FROM dim_orgao_superior as dos inner join fatoconvenios as fc inner join dim_ug_concedente as ugc on ugc.sk_ug_concedente = fc.k_UG_Concedente inner join dim_orgao_concedente as doc on doc.sk_orgao_concedente = fc.k_Orgao_Concedente inner join dimdata as dt on dos.sk_orgao_superior = fc.k_Orgao_Superior and dt.keyData = fc.k_Data and dt.ano_id >= "+ filtro_2 +" and dt.ano_id <= " + filtro_3 + " inner join dim_municipio as dm on dm.sk_municipio = fc.k_Municipio and dm.UF ="+"'"+str(filtro_1)+"'"
+
+    if filtro_4 != None:
+        consulta_sql_2 += " and dos.NOME_ORGAO_SUPERIOR ="+"'"+filtro_4+"'"
+
+    if filtro_5 != None:
+        consulta_sql_2 += " and doc.NOME_ORGAO_CONCEDENTE ="+"'"+filtro_5+"'"
+
+    if filtro_6 != None:
+       consulta_sql_2 += " and ugc.NOME_UG_CONCEDENTE ="+"'"+filtro_6+"'"
 
     consulta_sql_2+= " GROUP BY dos.NOME_ORGAO_SUPERIOR ORDER BY valor_convenio DESC"
 
